dc6801aa746e34b35a76ebcb41c5a3f234cda3f6
Divyesh Chhabra <divusurat26@gmail.com>
2024-07-07 16:37:35 +0530

    Implemented Components

---
+++ src/components/codeEditor.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+const CodeEditor = () => {
+    return (
+        <textarea
+            id="codeEditor"
+            placeholder="Start Coding..."
+        />
+    );
+}
+
+export default CodeEditor;
---
+++ src/components/title.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Title = () => {
+    return (
+        <React.Fragment>
+            <h1>Code Editor</h1>
+            <p>A simple no-frills code editor with syntax highlighting.</p>
+            <p>Happy Coding!</p>
+        </React.Fragment>
+    );
+}
+
+export default Title;
